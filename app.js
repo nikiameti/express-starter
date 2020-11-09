f90886103f79239284bb12c40384bebc7b266c89
besnik <besnik_ameti@drivosity.com>
2020-11-09 16:21:57 +0100

    socket support

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = 3000;
 const bootstrap = require('./config/bootstrap')();
-
+app.use(cors());
 app.use('/',bootstrap.getRoutes());
 
+app.get("/", async (req,res)=>{
+  res.sendFile(__dirname + '/client/index.html')
+})
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+const socket = require('./socket')(server)
